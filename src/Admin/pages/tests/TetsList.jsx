2ed19ce7399c709d3b2f372f9b28063d6b5be873
unknown <mailtosarantg@gmail.com>
2024-11-19 14:43:26 +0800

    image

--- src/Admin/pages/tests/TetsList.jsx
+++ src/Admin/pages/tests/TetsList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import moreIcon from "../../Assets/Images/more.png";
-import searchIcon from "../../Assets/Images/search.png";
+import moreIcon from "../../assets/Images/more.png";
+import searchIcon from "../../assets/Images/search.png";
 import { useNavigate } from 'react-router-dom';
 import TestDetails from './TestDetails';
 
