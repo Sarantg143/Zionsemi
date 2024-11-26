0d7702dc93bb0b30fb67bcc7d099284dc4ac424a
SARAN TG <113160601+Sarantg143@users.noreply.github.com>
2024-11-26 23:49:05 +0530

    Update Edit.jsx

--- src/Admin/pages/courses/edit-course/Edit.jsx
+++ src/Admin/pages/courses/edit-course/Edit.jsx
@@ -7,7 +7,7 @@ import Nolesson from "../../../assets/Images/no-lesson-illustration.svg";
 import BackIcon from "../../../assets/Images/left-arrow.png";
 import { useNavigate } from "react-router-dom";
 import NewLesson from "../new-course/NewLesson";
-import { deleteDegree, editDegree } from "../../../firebase/lessonApi";
+import { deleteDegree, editDegree } from "../../../firebase/degreeApi";
 import { toast } from "react-toastify";
 
 const Edit = ({ courseDetails }) => {
